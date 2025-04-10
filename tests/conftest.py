ae5c2f80a1b4de1a0707bce465b3e5f290986d62
Roman Varshavski <raman.varshavskiy@gmail.com>
2025-04-10 20:36:51 +0200

    add product.py, category.py, test_product.py, test_category.py

---
+++ tests/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+
+from src.category import Category
+from src.product import Product
+
+@pytest.fixture
+def category_1():
+    return Category(
+        name="Vegetables",
+        description = "Useful vegetables",
+        products = [
+            Product("огурцы", "огурцы для салата", 5.5, 3),
+            Product("помидоры", "помидоры для салата", 10.0, 4),
+            Product("лук", "лук для салата", 3.4, 5)
+        ]
+    )
+
+
+@pytest.fixture
+def category_2():
+    return Category(
+        name="Fruits",
+        description = "Sweet fruits",
+        products = [
+            Product("orange", "from Egypt", 6.5, 5),
+            Product("melon", "from Turkey", 8.8, 6),
+        ]
+    )
+
+@pytest.fixture
+def product():
+    return Product("orange", "from Egypt", 6.5, 5)
---
+++ tests/test_category.py
@@ -0,0 +1,11 @@
+
+
+def test_category_init(category_1, category_2):
+    assert category_1.name == "Vegetables"
+    assert category_1.description == "Useful vegetables"
+    assert len(category_1.products) == 3
+
+    assert category_1.category_count == 2
+    assert category_2.category_count == 2
+    assert category_1.product_count == 5
+    assert category_2.product_count == 5
